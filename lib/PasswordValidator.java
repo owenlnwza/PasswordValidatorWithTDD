b836cccbcd5dbeff5396503289a7cf734911546d
OWEN <owenworamet@gmail.com>
2025-07-05 22:14:34 +0700

    Add files via upload

--- lib/PasswordValidator.java
+++ lib/PasswordValidator.java
@@ -3,17 +3,36 @@ package lib;
 public class PasswordValidator {
 
     /**
-     * คุณจะต้องเขียน Javadoc ที่สมบูรณ์ที่นี่ในอนาคต
-     * เพื่ออธิบายกฎการทำงานของเมธอด
+     * @param รับ Password ที่เป็น String
+     * @return ค่า PasswordStrength INVALID และอื่นๆ
      */
     // TODO: แก้ไข return type ของเมธอดนี้ให้ถูกต้อง
-    public static void validate(String password) { // Function Type ให้เป็น PasswordStrength 
+    public static PasswordStrength validate(String password) { // Function Type ให้เป็น PasswordStrength 
         
-        // ส่วนของ Implementation ที่คุณต้องเขียน
-        // ...
+        if (password == null || password.length() < 8) {
+            return PasswordStrength.INVALID;
+        }
+        boolean Lower = false;
+        boolean Upper = false;
+        boolean Digit = false;
+        boolean Special = false;
+
+        for(char c : password.toCharArray()){
+            if (Character.isLowerCase(c)) {
+                Lower = true;
+            }else if (Character.isUpperCase(c)) {
+                Upper = true;
+            }else if (Character.isDigit(c)) {
+                Digit = true;
+            }else if (!Character.isLowerCase(c) && !Character.isUpperCase(c) && !Character.isDigit(c)) {
+                Special = true;
+            }
+        }
+
+        if (Lower && !Upper && !Digit && !Special) return PasswordStrength.WEAK;
+        if (Lower && Upper && Digit && !Special) return PasswordStrength.MEDIUM;
+        if (Lower && Upper && Digit && Special) return PasswordStrength.STRONG;
         
-        int minLength = 8 // TODO: มีอะไรขาดหายไปที่บรรทัดนี้?
-        
-        return null ; // TODO: การคืนค่านี้ถูกต้องหรือไม่?
+        return PasswordStrength.INVALID ; // TODO: การคืนค่านี้ถูกต้องหรือไม่?
     }
 }
--- lib/PasswordValidatorTest.java
+++ lib/PasswordValidatorTest.java
@@ -6,11 +6,39 @@ public class PasswordValidatorTest{
         System.out.println("--- Running Password Validator Tests ---");
         
         // Test Case 1: รหัสผ่านสั้นควรจะ INVALID
-        PasswordStrength result1 = PasswordValidator.validate("123");
+        String pw = "owen";
+        PasswordStrength result1 = PasswordValidator.validate(pw);
         if (result1 == PasswordStrength.INVALID) {
-            System.out.println("Test Case 1 Passed: Short password is INVALID.");
+            System.out.println("Test Case 1 Passed: "+pw+" Short password is INVALID.");
         } else {
-            System.out.println("Test Case 1 FAILED: Expected INVALID but got " + result1);
+            System.out.println("Test Case 1 FAILED: "+pw+" Expected INVALID but got " + result1);
+        }
+
+        // Test Case 2: รหัสผ่านพิมพ์เล็กทั้งหมด เป็น WEAK
+        pw = "owenowen";
+        PasswordStrength result2 = PasswordValidator.validate(pw);
+        if (result2 == PasswordStrength.WEAK) {
+            System.out.println("Test Case 2 Passed: "+pw+" Lower password is WEAK.");
+        } else {
+            System.out.println("Test Case 2 FAILED: "+pw+" Expected WEAK but got " + result2);
+        }
+
+        // Test Case 3: รหัสผ่านพิมพ์เล็ก พิมใหญ์ และ ตัวเลข เป็น MEDIUM
+        pw = "OWENza123";
+        PasswordStrength result3 = PasswordValidator.validate(pw);
+        if (result3 == PasswordStrength.MEDIUM) {
+            System.out.println("Test Case 3 Passed: "+pw+" Lower Upper and Digit password is MEDIUM.");
+        } else {
+            System.out.println("Test Case 3 FAILED: "+pw+" Expected MEDIUM but got " + result3);
+        }
+
+        // Test Case 4: รหัสผ่านพิมพ์เล็ก พิมใหญ์ ตัวเลข และ อักษรพิเศษ เป็น MEDIUM
+        pw = "OWENza123_*";
+        PasswordStrength result4 = PasswordValidator.validate(pw);
+        if (result4 == PasswordStrength.STRONG) {
+            System.out.println("Test Case 4 Passed: "+pw+" Lower Upper Digit and Special password is STRONG.");
+        } else {
+            System.out.println("Test Case 4 FAILED: "+pw+" Expected STORNG but got " + result4);
         }
 
         // --- เขียน Test Case อื่นๆ ต่อ ---
